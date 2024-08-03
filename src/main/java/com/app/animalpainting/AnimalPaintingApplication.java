8829f17e0ee8e6b05df2e24213557c3ab4c09214
JossguerreroP <josguerrerop@udistrital.edu.co>
2024-08-03 00:21:15 -0500

    Initial commit

---
+++ src/main/java/com/app/animalpainting/AnimalPaintingApplication.java
@@ -0,0 +1,13 @@
+package com.app.animalpainting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalPaintingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalPaintingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/animalpainting/AnimalPaintingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.animalpainting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalPaintingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
